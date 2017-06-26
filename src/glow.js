2a38fda9026d4ef9a96580a7ec36965fb3863d5d
Alexis Hope <alexis@cvofhope.com>
2017-06-27 08:22:22 +1000

    convert colour functions to es5

--- src/glow.js
+++ src/glow.js
@@ -1,58 +1,65 @@
 
-function hslToRgb(hsl){
-    var h, s, l,
-        r, g, b,
-        temp1, temp2;
-    // clone array
-    hsl = hsl.slice(0)
 
-    hsl = degPercPercToHsl(hsl)
-    h = hsl[0]
-    s = hsl[1]
-    l = hsl[2]
+class Glow {
+  
+  constructor() {
+    this.cache = {}
+  }
+  
+  hslToRgb(hsl){
+      var h, s, l,
+          r, g, b,
+          temp1, temp2;
+      // clone array
+      hsl = hsl.slice(0)
 
-    // greyscale
-    if (s == 0) {
-        r = l * 255
-        g = l * 255
-        b = l * 255
-    } else {
-        temp2 = (l < 0.5) ? l * (1 + s) : (l + s) - (s * l)
-        temp1 = 2 * l - temp2
+      hsl = degPercPercToHsl(hsl)
+      h = hsl[0]
+      s = hsl[1]
+      l = hsl[2]
 
-        r = Math.round(255 * hueToRgb(temp1, temp2, h + ( 1 / 3 )))
-        g = Math.round(255 * hueToRgb(temp1, temp2, h))
-        b = Math.round(255 * hueToRgb(temp1, temp2, h - 0.3333))
-    }
+      // greyscale
+      if (s == 0) {
+          r = l * 255
+          g = l * 255
+          b = l * 255
+      } else {
+          temp2 = (l < 0.5) ? l * (1 + s) : (l + s) - (s * l)
+          temp1 = 2 * l - temp2
 
-    return [r, g, b];
-}
+          r = Math.round(255 * hueToRgb(temp1, temp2, h + ( 1 / 3 )))
+          g = Math.round(255 * hueToRgb(temp1, temp2, h))
+          b = Math.round(255 * hueToRgb(temp1, temp2, h - 0.3333))
+      }
 
-function hueToRgb(temp1, temp2, hue) {
-    var rgbValue = 0;
-    if (hue < 0) {
-        hue += 1
-    }
-    if (hue > 1) {
-        hue -= 1
-    }
+      return [r, g, b];
+  }
+  
+  hueToRgb(temp1, temp2, hue) {
+      var rgbValue = 0;
+      if (hue < 0) {
+          hue += 1
+      }
+      if (hue > 1) {
+          hue -= 1
+      }
 
-    if (6 * hue < 1 ) { // red
-        rgbValue = temp1 + (temp2 - temp1) * 6 * hue
-    } else if (2 * hue < 1 ) { // green
-        rgbValue = temp2
-    } else if (3 * hue < 2 ) { // blue
-        rgbValue = temp1 + (temp2 - temp1) * (0.6666 - hue) * 6
-    }
-    return rgbValue;
-}
-
-function hexToHsl(hex) {
+      if (6 * hue < 1 ) { // red
+          rgbValue = temp1 + (temp2 - temp1) * 6 * hue
+      } else if (2 * hue < 1 ) { // green
+          rgbValue = temp2
+      } else if (3 * hue < 2 ) { // blue
+          rgbValue = temp1 + (temp2 - temp1) * (0.6666 - hue) * 6
+      }
+      return rgbValue;
+  }
+ 
+  hexToHsl(hex) {
     var rgb = hexToRgb(hex)
     return rgbToHsl(rgb)
-}
-
-function hslToDegPercPerc(hsl) {
+  }
+  
+  hslToDegPercPerc(hsl) {
     hsl[0] = Math.round(hsl[0] * 60)
     if (hsl[0] < 0) {
         hsl[0] += 360
@@ -61,17 +68,17 @@ function hslToDegPercPerc(hsl) {
     hsl[1] = Math.round(hsl[1] * 100)
     hsl[2] = Math.round(hsl[2] * 100)
     return hsl
-}
+  }
 
-function degPercPercToHsl(hsl) {
+  degPercPercToHsl(hsl) {
     hsl[0] /= 360;
     hsl[1] /= 100;
     hsl[2] /= 100;
 
     return hsl;
-}
-
-function rgbToHsl(rgb) {
+  }
+  
+  rgbToHsl(rgb) {
     var max = 0, min = 0, maxAdd = 0, maxSub = 0,
         h=0, s=0, l=0
 
@@ -99,26 +106,26 @@ function rgbToHsl(rgb) {
                 h = (rgb[2] - rgb[0]) / maxSub + 2
                 break
             case rgb[2]: // blue
-                h = (rgb[0] - rgb[1]) / maxSub + 4;
+                h = (rgb[0] - rgb[1]) / maxSub + 4
                 break
         }
     }
 
     return hslToDegPercPerc([h, s, l])
-}
-
-function hexToRgb(hex) {
-
+  }
+  
+  hexToRgb(hex) {
     hex = /[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}/.exec(hex)
     if (hex === null) {
-        throw "not a valid hex value"
+      throw "not a valid hex value"
     }
     hex = hex[0].match(/.{1,2}/g)
 
     return hex.map(function (v) {
-        return parseInt(v, 16)
+      return parseInt(v, 16)
     })
-
+  }
+  
 }
 
 //var t = hexToHsl('#359ea7')
