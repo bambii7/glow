c163fc311827fdec2b7077dbeea275cbfb926ada
Alexis Hope <alexis@cvofhope.com>
2017-07-06 09:27:36 +1000

    add test for error hexToRgb error

--- test/core/core-test.js
+++ test/core/core-test.js
@@ -4,12 +4,20 @@ const it         = require('mocha').it
 const expect     = require('chai').expect
 var Glow       = require('../../build/glow-v1.0.0').Glow
 
-describe('calculator', function () {
+describe('hexToRgb', function () {
 
   it('should convert RGB to hsl values', function () {
     let glow = new Glow()
     let hsl = glow.hexToRgb('#359ea7')
     expect(hsl).to.deep.equal([53, 158, 167])
+  })
+  
+  it('should throw an error if no hex value given', function () {
+    let glow = new Glow()
+    var fn = function () {
+      glow.hexToRgb('ZZZ') 
+    }
+    expect(fn).to.throw()
   })
   
 });
