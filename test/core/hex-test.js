bfe908fe84d96f9ace40160b29fade2f249bef1a
Alexis Hope <alexis@cvofhope.com>
2017-08-10 10:25:39 +1000

    add hex unit tests

---
+++ test/core/hex-test.js
@@ -0,0 +1,35 @@
+"use strict"
+const describe   = require('mocha').describe;
+const it         = require('mocha').it
+const expect     = require('chai').expect
+var Hex       = require('../../build/glow').Hex
+
+describe('Hex', function () {
+
+  it('should convert hex to rgb values', function () {
+    let hex = new Hex('#359ea7')
+    let rgb = hex.toRgb()
+    expect(rgb).to.deep.equal([53, 158, 167])
+  })
+  
+//  it('should expand 3 digit hex values', function () {
+//    let glow = new Glow()
+//    let expanded = glow.stringToHex('#09C')
+//    expect(expanded).to.deep.equal('0099CC')
+//  })  
+//  
+//  it('should not expand 6 digit hex values', function () {
+//    let glow = new Glow()
+//    let expanded = glow.stringToHex('#0099CC')
+//    expect(expanded).to.deep.equal('0099CC')
+//  })
+//  
+//  it('should throw an error if no hex value given', function () {
+//    let glow = new Glow()
+//    var fn = function () {
+//      glow.hexToRgb('ZZZ')
+//    }
+//    expect(fn).to.throw()
+//  })
+  
+});
