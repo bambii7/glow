88be1a8154e9343b383247f1025c9307cf12b2f7
Alexis Hope <alexis@cvofhope.com>
2017-08-11 09:04:50 +1000

    test if colour is valid before setting

--- src/color-space/Color.js
+++ src/color-space/Color.js
@@ -2,6 +2,10 @@ const regex = /(\d+),(\d+),(\d+)/
 
 export default class Color {
   
+  constructor(str = '') {
+    this.value = Color.isValid(str) ? Color.parseString(str) : null
+  }
+  
   static parseString(str) {
     let res = regex.exec(str)
     this.value = res.shift().slice(0)
