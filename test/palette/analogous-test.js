600a077a6c4136e59f7009d25de02248512690fb
Alexis Hope <alexis@cvofhope.com>
2017-08-18 20:08:43 +1000

    update analogous test description

--- test/palette/analogous-test.js
+++ test/palette/analogous-test.js
@@ -7,7 +7,7 @@ const HSL        = require('../../build/glow').HSL
 
 describe('Analogous', () => {
 
-  it('should set a value on constructor parameter', () => {
+  it('should generate an analogous color palette', () => {
     let hsl = new HSL('hsl(236,78,57)')
     let analogous = new Analogous(hsl)
     expect(analogous.toPalette()).to.deep.equal(["hsl(201,78,57)", "hsl(191,78,57)", "hsl(181,78,57)", "hsl(172,78,57)", "hsl(163,78,57)"])
