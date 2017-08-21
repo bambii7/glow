6fd22ff194ddaab99e11a3ddae9e1be5316f6093
Alexis Hope <alexis@cvofhope.com>
2017-08-21 21:38:24 +1000

    add log function to palette

--- src/palette/Palette.js
+++ src/palette/Palette.js
@@ -1,5 +1,15 @@
+import HSL from '../color-space/HSL'
+
 export default class Palette {
   constructor(hsl) {
     this.value = hsl
-  } 
+  }
+  
+  log() {
+    let palette = this.toPalette()
+    palette.forEach((color) => {
+      let hex = new HSL(color).toHex()
+      console.log('%c Glow!', `background: ${hex}; color: #FFF`);  
+    })
+  }
 }
